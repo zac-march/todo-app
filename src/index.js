27276b81bbe11a9f515b4f57bfc4c4c5e4663a69
Zac <121649297+zac-march@users.noreply.github.com>
2023-03-19 22:00:44 +1100

    Add persistent storage error checking

--- src/index.js
+++ src/index.js
@@ -4,22 +4,55 @@ import { TodoListCollection } from "./components/TodoListCollection";
 import { TodoListCollectionScreenController } from "./components/TodoListCollectionScreenController";
 import "./style.css";
 
-let todoCollection;
-if (localStorage.getItem("collection") === "null") {
-  todoCollection = new TodoListCollection();
-  todoCollection.addDemoList();
-} else {
-  let todoListCollectionProto = new TodoListCollection();
-  let todoListProto = new TodoList();
-  let todoProto = new Todo();
-  todoCollection = JSON.parse(localStorage.getItem("collection"));
-  Object.setPrototypeOf(todoCollection, todoListCollectionProto);
-  for (let todoList of todoCollection.collection) {
-    Object.setPrototypeOf(todoList, todoListProto);
-    for (let todo of todoList.items) {
-      Object.setPrototypeOf(todo, todoProto);
-    }
-  }
-}
+let todoCollection = getTodoCollection();
 
 TodoListCollectionScreenController(todoCollection);
+
+function getTodoCollection() {
+  if (
+    localStorage.getItem("collection") === "null" ||
+    !storageAvailable("localStorage")
+  ) {
+    todoCollection = new TodoListCollection();
+    todoCollection.addDemoList();
+  } else {
+    todoCollection = JSON.parse(localStorage.getItem("collection"));
+    Object.setPrototypeOf(todoCollection, new TodoListCollection());
+    for (let todoList of todoCollection.collection) {
+      Object.setPrototypeOf(todoList, new TodoList());
+      for (let todo of todoList.items) {
+        Object.setPrototypeOf(todo, new Todo());
+      }
+    }
+  }
+  return todoCollection;
+}
+
+function storageAvailable(type) {
+  // Function plucked from the Web Storage API docs
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
